dd0049bb9cef42b19f06f382a38de0f6d8872f9d
Your Name <you@example.com>
2020-03-30 20:57:00 +0530

    signal added

---
+++ 20_signal_concept/06_t_kill.c
@@ -0,0 +1,19 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : 06_t_kill.c
+ * Created on    : Wednesday 22 January 2020 09:36:36 PM IST
+ * Last modified : Wednesday 22 January 2020 09:38:27 PM IST
+ * Description   : 
+ * ***********************************************************************/
+
+#include "utilities.h"
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+	
+
+
+	exit(EXIT_SUCCESS);
+}
+
